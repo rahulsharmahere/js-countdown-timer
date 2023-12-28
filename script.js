e1964be841b06f8a6921554661e4ba5c69be4965
Rahul Sharma <track.rahulsharma@gmail.com>
2023-12-28 21:05:03 +0530

    This is a Count Down Timer In JS

---
+++ script.js
@@ -0,0 +1,33 @@
+const endDate = document.getElementById('end-date').innerText;
+const inputs = document.querySelectorAll("input")
+
+const clock = () => {
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff =(end-now)/1000
+
+
+    if (diff < 0){
+        console.log("Done")
+        clearInterval(clearInt);
+        return;
+        
+
+    }
+
+    // COnvert In days
+    inputs[0].value = (Math.floor(diff/3600/24))
+    inputs[1].value = (Math.floor((diff/3600) % 24))
+    inputs[2].value = (Math.floor(diff/ 60 % 60))
+    inputs[3].value = (Math.floor(diff% 60))
+    
+}
+
+
+clock()
+
+
+const clearInt = setInterval(() => {
+    clock()
+}, 1000);
+ 
